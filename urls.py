61a0ff4a8eb5ef65bf6d4f43ab0c9ba6c0e1e5be
amarala931 <amarala931@g.educaand.es>
2026-06-01 02:08:22 +0200

    Add standings, events, and ranking URLs to urls.py

--- urls.py
+++ urls.py
@@ -1,1 +1,3 @@
 STANDINGS = "https://www.sofascore.com/api/v1/unique-tournament/16/season/58210/standings/total"
+EVENTS = "https://www.sofascore.com/api/v1/unique-tournament/16/scheduled-events/2026-06-25"
+RANKING = "https://www.sofascore.com/api/v1/unique-tournament/16/season/58210/power-rankings/round/133"

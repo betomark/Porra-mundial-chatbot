867ae2622225c15340de5552c8a030320e68763a
betomark <almark16@gmail.com>
2026-06-02 20:05:22 +0200

    Refactor API URLs in urls.py to use a base URL for better maintainability

--- urls.py
+++ urls.py
@@ -1,3 +1,7 @@
-STANDINGS = "https://www.sofascore.com/api/v1/unique-tournament/16/season/58210/standings/total"
-EVENTS = "https://www.sofascore.com/api/v1/unique-tournament/16/scheduled-events/2026-06-25"
-RANKING = "https://www.sofascore.com/api/v1/unique-tournament/16/season/58210/power-rankings/round/133"
+SOFASCORE_BASE_URL = "https://www.sofascore.com/api/v1"
+
+STANDINGS = f"{SOFASCORE_BASE_URL}/unique-tournament/{{tournament_id}}/season/{{season_id}}/standings/total"
+EVENTS = f"{SOFASCORE_BASE_URL}/unique-tournament/{{tournament_id}}/scheduled-events/{{date}}"
+RANKING = f"{SOFASCORE_BASE_URL}/unique-tournament/{{tournament_id}}/season/{{season_id}}/power-rankings/round/{{round_id}}"
+TEAM_LASTS_MATCHES = f"{SOFASCORE_BASE_URL}/team/{{team_id}}/events/last/0"
+TEAM_RECENT_PERFORMANCE = f"{SOFASCORE_BASE_URL}/team/{{team_id}}/performance"

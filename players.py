6a42acc431c1336ac7f1139dc51e56519202aeba
betomark <almark16@gmail.com>
2026-06-05 14:29:13 +0200

    Refactor get_player_total_stats function to improve player statistics retrieval

--- players.py
+++ players.py
@@ -1,4 +1,3 @@
-import json
 import utils.sofascore_client
 import urls
 
@@ -11,3 +10,27 @@ def get_player_season_stats(player_id, tournament_id, season_id):
     url = urls.PLAYER_SEASON_STATS.format(player_id=player_id, tournament_id=tournament_id, season_id=season_id)
     sofascore_client = utils.sofascore_client.SofascoreClient()
     return sofascore_client.get(url)
+
+def get_player_total_stats(player_id):
+    player_seasons = get_player_seasons(player_id)
+    player_stats = {}
+    for tournament in player_seasons["uniqueTournamentSeasons"]:
+        tournament_id = tournament["uniqueTournament"]["id"]
+        tournament_name = tournament["uniqueTournament"]["name"]
+        tournament_place = tournament["uniqueTournament"]["category"]["name"]
+        player_stats[tournament_name] = {
+            "tournament_id": tournament_id,
+            "tournament_place": tournament_place,
+            "seasons": {}
+        }
+        for season in tournament["seasons"]:
+            season_id = season["id"]
+            season_name = season["name"]
+            season_year = season["year"]
+            player_stats[tournament_name]["seasons"][season_name] = {
+                "season_id": season_id,
+                "season_year": season_year
+            }
+            player_stats[tournament_name]["seasons"][season_name] = get_player_season_stats(player_id, tournament_id, season_id)
+
+    return player_stats

e9c45d40d397bfd202e08398716ec9cdbc5f12fe
betomark <almark16@gmail.com>
2026-06-05 11:44:30 +0200

    Add new player and team endpoints to urls.py for enhanced API access

--- urls.py
+++ urls.py
@@ -1,7 +1,20 @@
 SOFASCORE_BASE_URL = "https://www.sofascore.com/api/v1"
 
+#tournament endpoints
 STANDINGS = f"{SOFASCORE_BASE_URL}/unique-tournament/{{tournament_id}}/season/{{season_id}}/standings/total"
 EVENTS = f"{SOFASCORE_BASE_URL}/unique-tournament/{{tournament_id}}/scheduled-events/{{date}}"
+
+#match endpoints
+MATCH_ODDS = f"{SOFASCORE_BASE_URL}/event/{{game_id}}/odds/1/all"
+
 RANKING = f"{SOFASCORE_BASE_URL}/unique-tournament/{{tournament_id}}/season/{{season_id}}/power-rankings/round/{{round_id}}"
+
+#team endpoints
 TEAM_LASTS_MATCHES = f"{SOFASCORE_BASE_URL}/team/{{team_id}}/events/last/0"
 TEAM_RECENT_PERFORMANCE = f"{SOFASCORE_BASE_URL}/team/{{team_id}}/performance"
+TEAM_STATS = f"{SOFASCORE_BASE_URL}/team/{{team_id}}/unique-tournament/{{tournament_id}}/season/{{season_id}}/statistics/overall"
+SQUAD = f"{SOFASCORE_BASE_URL}/team/{{team_id}}/players"
+
+#player endpoints
+PLAYER_SEASONS = f"{SOFASCORE_BASE_URL}/player/{{player_id}}/statistics/seasons"
+PLAYER_SEASON_STATS = f"{SOFASCORE_BASE_URL}/player/{{player_id}}/unique-tournament/{{tournament_id}}/season/{{season_id}}/statistics/overall"

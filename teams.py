03cb6de2e94dbe5918ee87f3e02aae43cf2dbae3
betomark <almark16@gmail.com>
2026-06-06 19:14:23 +0200

    Refactor team statistics functions into Team class for better encapsulation

--- teams.py
+++ teams.py
@@ -10,23 +10,26 @@ def get_teams(standings_dict):
 
     with open("data/teams.json", "w", encoding="utf-8") as f:
         json.dump(datos_equipos, f, indent=4, ensure_ascii=False)
+class Team:
+    def __init__(self, team_id):
+        self.team_id = team_id
 
-def get_team_stats(team_id, tournament_id, season_id):
-    url = urls.TEAM_STATS.format(team_id=team_id, tournament_id=tournament_id, season_id=season_id)
-    sofascore_client = utils.sofascore_client.SofascoreClient()
-    return sofascore_client.get(url)
+    def get_team_stats(self, tournament_id, season_id):
+        url = urls.TEAM_STATS.format(team_id=self.team_id, tournament_id=tournament_id, season_id=season_id)
+        sofascore_client = utils.sofascore_client.SofascoreClient()
+        return sofascore_client.get(url)
 
-def get_team_recent_performance(team_id):
-    url = urls.TEAM_RECENT_PERFORMANCE.format(team_id=team_id)
-    sofascore_client = utils.sofascore_client.SofascoreClient()
-    return sofascore_client.get(url)
+    def get_team_recent_performance(self):
+        url = urls.TEAM_RECENT_PERFORMANCE.format(team_id=self.team_id)
+        sofascore_client = utils.sofascore_client.SofascoreClient()
+        return sofascore_client.get(url)
 
-def get_team_last_matches(team_id):
-    url = urls.TEAM_LASTS_MATCHES.format(team_id=team_id)
-    sofascore_client = utils.sofascore_client.SofascoreClient()
-    return sofascore_client.get(url)
+    def get_team_last_matches(self):
+        url = urls.TEAM_LASTS_MATCHES.format(team_id=self.team_id)
+        sofascore_client = utils.sofascore_client.SofascoreClient()
+        return sofascore_client.get(url)
 
-def get_team_squad(team_id):
-    url = urls.SQUAD.format(team_id=team_id)
-    sofascore_client = utils.sofascore_client.SofascoreClient()
-    return sofascore_client.get(url)
+    def get_team_squad(self):
+        url = urls.SQUAD.format(team_id=self.team_id)
+        sofascore_client = utils.sofascore_client.SofascoreClient()
+        return sofascore_client.get(url)
